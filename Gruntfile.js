efd6de4325388911fe7b8ce8faeaa2916a235ab5
Tarun Desikan <tdesikan@gmail.com>
2014-05-23 11:53:23 -0700

    css & js minification via grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,69 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        
-        concat: {
-            css: {
-                src: [
-                    'assets/styles/*'
-                ],
-                dest: 'assets/styles/combined.css'
-            },
-            js: {
-                src: [
-                    'assets/scripts/*'
-                ],
-                dest: 'assets/scripts/combined.js'
-            }
-        },
-        
-        cssmin: {
-            css: {
-                src: 'assets/styles/combined.css',
-                dest: 'assets/styles/combined.min.css'
-            }
-        },
-        
-        uglify: {
-            js: {
-                files: {
-                    'assets/scripts/combined.min.js': ['assets/scripts/combined.js']
-                }
-            }
-        },
-    });
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    dirs: {
+      css: 'assets/styles',
+      js: 'assets/scripts'
+    },
+    
+    clean: [
+      '<%= dirs.css %>/combined.*',
+      '<%= dirs.js %>/combined.*'
+    ],
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    concat: {
+      css: {
+        src: [
+            '<%= dirs.css %>/reset.css', '<%= dirs.css %>/monkey*', '<%= dirs.css %>/*'
+        ],
+        dest: '<%= dirs.css %>/combined.css'
+      },
+      js: {
+        src: [
+            '<%= dirs.js %>/jquery.js', '<%= dirs.js %>/jquery.*', '<%= dirs.js %>/*'
+        ],
+        dest: '<%= dirs.js %>/combined.js'
+      }
+    },
+    
+    cssmin: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      css: {
+        src: '<%= dirs.css %>/combined.css',
+        dest: '<%= dirs.css %>/combined.min.css'
+      }
+    },
+    
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      js: {
+        files: {
+          '<%= dirs.js %>/combined.min.js': ['<%= dirs.js %>/combined.js']
+        }
+      }
+    },
 
-    grunt.registerTask('default', ['concat:css', 'cssmin:css', 'concat:js', 'uglify:js']);
+    processhtml: {
+      dist: {
+        files: {
+          'index.html': ['index.html']
+        }
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-processhtml');
+
+  grunt.registerTask('default', ['clean', 'concat:css', 'cssmin:css', 'concat:js', 'uglify:js', 'processhtml:dist']);
 };
